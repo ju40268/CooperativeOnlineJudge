e8b007979f3179b5bfbde8d10f9e4f038d5c17ee
ju40268 <ju40268@gmail.com>
2018-08-07 23:58:10 -0500

    editor working!

--- oj-client/src/app/components/editor/editor.component.ts
+++ oj-client/src/app/components/editor/editor.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import 'ace-builds/src-noconflict/ace';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/theme-eclipse';
+// import 'ace-builds/src-noconflict/snippets/sql';
+import 'ace-builds/src-noconflict/mode-java';
 
+declare var ace: any;
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  constructor() { }
+    private _divIdEditor: string = 'editor';
+    defaultContent = {
+      'Java' : `public class Solution {
+    public static void main(String args[]) {
+        // start your code here.
+    }
+}`
+    }
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    get divIdEditor(): string {
+      return this._divIdEditor;
+    }
+
+    ngOnInit() {
+      var editor = ace.edit(this._divIdEditor);
+      editor.setTheme('ace/theme/monokai');
+      editor.getSession().setMode('ace/mode/java')
+      editor.setValue(this.defaultContent['Java']);
+      editor.$blockScrolling = Infinity;
+    }
 
 }
